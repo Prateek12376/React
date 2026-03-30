56971f9534a383aa1c618b434c501009ad99b912
Prateek12376 <prateekv700@gmail.com>
2026-03-30 22:50:19 +0530

    counter project added

---
+++ counterP/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  const[counter,setCounter]=useState(10)
+
+  //let counter = 5
+
+  const IncreaseV = ()=>{
+    console.log("value added", counter)
+    // counter=counter+1
+    setCounter(counter<20?counter+1: counter)
+  }
+
+  const decreaseV = ()=>{
+    console.log("value subtracted",counter)
+    // counter=counter-1
+    setCounter(counter>0?counter-1: counter)
+  }
+
+  return (
+    <>
+      <h1>Counter function by useState</h1>
+      <h2>counter value : {counter}</h2>
+      <button
+      onClick={IncreaseV}>Increase value</button>
+      <br/>
+      <button
+      onClick={decreaseV}>Decrease value</button>
+    </>
+  )
+}
+
+export default App
