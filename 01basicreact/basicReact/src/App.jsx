394a99d688e375a6833be9c98f68e28d5e699e35
Prateek12376 <prateekv700@gmail.com>
2026-03-30 22:55:16 +0530

    added all react projects

---
+++ 01basicreact/basicReact/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from "./chai"
+
+function App() {
+
+  return (
+    <>  //fragment , jsx allow single elem, so we have beenclosed all elm in single div
+      <Chai/>
+      <h1>hello evry one</h1>
+      <p>kee haal chaal </p>
+    </>
+  )
+}
+
+export default App
